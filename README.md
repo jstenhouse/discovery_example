752fc5d0a7c21921c6ea525cb4aace8f48ffd40e
Jason Stenhouse <jason.stenhouse@gmail.com>
2016-01-01 14:03:42 +0100

    initial configuration

---
+++ README.md
@@ -0,0 +1,3 @@
+## Discovery Example
+
+Going to play with [Spring-Boot](http://projects.spring.io/spring-boot/) some more and various discovery tools: [Consul](https://www.consul.io/) and [etcd](https://github.com/coreos/etcd).
---
+++ src/main/java/com/stenhouse/discovery/Application.java
@@ -0,0 +1,19 @@
+package com.stenhouse.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by jason on 12/31/15.
+ */
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
