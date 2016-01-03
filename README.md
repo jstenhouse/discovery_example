5048af99e0c9c009d3e6b258703dfea03b89c9af
Jason Stenhouse <jason.stenhouse@gmail.com>
2016-01-03 22:36:46 +0100

    initial rest template

--- README.md
+++ README.md
@@ -23,13 +23,13 @@ http://localhost:8080/number
 ```
 
 ```
-docker run --name discovery_example_0 --rm -p 8080:8080 -e SERVER_PORT=8080 jstenhouse/discovery_example:latest
+docker run --name discovery_example_0 -d -p 8080:8080 -e SERVER_PORT=8080 jstenhouse/discovery_example:latest
 ```
 
 ```
-docker run --name discovery_example_1 --rm -p 8081:8081 -e SERVER_PORT=8081 jstenhouse/discovery_example:latest
+docker run --name discovery_example_1 -d -p 8081:8081 -e SERVER_PORT=8081 jstenhouse/discovery_example:latest
 ```
 
 ```
-docker run --name discovery_example_2 --rm -p 8082:8082 -e SERVER_PORT=8082 jstenhouse/discovery_example:latest
+docker run --name discovery_example_2 -d -p 8082:8082 -e SERVER_PORT=8082 jstenhouse/discovery_example:latest
 ```
--- src/main/java/com/stenhouse/discovery/controller/NumberWordController.java
+++ src/main/java/com/stenhouse/discovery/controller/NumberWordController.java
@@ -1,8 +1,11 @@
 package com.stenhouse.discovery.controller;
 
+import com.stenhouse.discovery.model.Number;
 import com.stenhouse.discovery.model.NumberWord;
+import com.stenhouse.discovery.model.Word;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -14,6 +17,11 @@ public class NumberWordController {
 
     @RequestMapping(value="/number-word", method=GET)
     public NumberWord getNumberWord() {
-        return new NumberWord(42, "liminal");
+        final RestTemplate restTemplate = new RestTemplate();
+
+        Number number  = restTemplate.getForObject("http://localhost:8080", Number.class);
+        Word word = restTemplate.getForObject("http://localhost:8081", Word.class);
+
+        return new NumberWord(number.getValue(), word.getValue());
     }
 }
